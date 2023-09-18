f9007665bccf80fc01b3e50a025a291176ba04d9
jackjg123 <131559096+jackjg123@users.noreply.github.com>
2023-09-18 11:00:57 -0400

    Added playRound function to compare user choice to computer choice.

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -3,8 +3,28 @@ var wins = 0;
 var computerWins = 0;
 var ties = 0;
 
+// Computer chooses randomly between choices
 function computerChoice() {
   var choices = ['R', 'P', 'S'];
   var randomChoice = Math.floor(Math.random() * choices.length);
-  return choiices[randomChoice];
+  return choices[randomChoice];
+}
+
+// Compare user choice to computer choice
+function playRound(userSelection) {
+  var computerSelection = computerChoice();
+
+  if (userSelection === computerSelection) {
+    ties++;
+    alert("It's a tie!");
+  } else if (
+    (userSelection === 'R' && computerSelection === 'S') ||
+    (userSelection === 'P' && computerSelection === 'R') ||
+    (userSelection === 'S' && computerSelection === 'P')
+  ) {
+    wins++;
+    alert('Congrats! You got the W!');
+  } else {
+    alert('Sorry, take this L');
+  }
 }
